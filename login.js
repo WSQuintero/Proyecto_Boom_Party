178b1abbd1ab5463c25c2e4d852a53c7ebb7edcd
Michael Caro <mcarom1206@gmail.com>
2023-05-05 23:56:29 -0500

    Cambio de logica del codigo
    En el codigo lo explico a detalle.
    Tambien se cambia la logica para que no se utilice el var

--- login.js
+++ login.js
@@ -6,39 +6,47 @@ button.addEventListener("click", (e) => {
   };
 
   const usuariosAlmacenados = JSON.parse(localStorage.getItem("baseUsuarios"));
-  
-  let usuarioEncontrado = false;
-  let contrasenaIncorrecta = false;
+
+  /* Un find para encontrar al usuario, donde se convierte en true si lo encuentra y undefined si no */
+  const usuarioEncontrado = usuariosAlmacenados.find(
+    (usuarioActual) => usuario.user === usuarioActual.user
+  );
+
+  /* Error que puede aparecer:
+      -si el usuario se encuentra registrado devolvera false */
   let usuarioNoRegistrado = true;
 
-  /* Se recorre el array y se define cuales datos se encuentran y cuales no */
-  for (let i = 0; i < usuariosAlmacenados.length; i++) {
-    const usuarioActual = usuariosAlmacenados[i];
-    if (usuario.user === usuarioActual.user) {
-      usuarioNoRegistrado = false;
-      if (usuario.pass === usuarioActual.pass) {
-        usuarioEncontrado = true;
-        break;
-      } else {
-        contrasenaIncorrecta = true;
-      }
-    }
-  }
+  /* Mensaje que se mostrara cuando haya errores */
+  let mensajeError = "";
 
-  /* condiciones para iniciar sesion */
-  switch (true) {
-    case usuarioEncontrado:
+  /* Si usuario encontrado es true entra al if. En caso contrario colocara el mensaje de usuario no registrado en mensajeError 
+     cuando se entra al if:
+       -se convierte en usuario registrado, dando false al error de usuarioNoRegistrado
+       -se revisa la contraseña y si concuerda, entra a la pagina principal. En caso contrario colocara el mensaje de contraseña incorrecta en mensajeError */
+  if (usuarioEncontrado) {
+    usuarioNoRegistrado = false;
+    if (usuario.pass === usuarioEncontrado.pass) {
       window.location.replace("Pagina Principal.html");
-      break;
-    case contrasenaIncorrecta:
-      var mensajeError = "Contraseña incorrecta";
-      break;
-    case usuarioNoRegistrado:
-      var mensajeError = "Usuario no registrado";
-      break;
+    } else {
+      mensajeError = "Contraseña incorrecta";
+    }
+  } else {
+    mensajeError = "Usuario no registrado";
   }
 
-  /* Mensaje cuando los datos no son correctos */
+  /* Colocar mensaje de error debajo del formulario:
+      -El mensajeError fue definido en el if anterior, asi que entra a este if
+      -se crea elemento <p> en errorMensaje para mostrar el mensaje despues
+      -se guarda el mensaje del error con .textcontent para el <p>
+      -se crea la clase ".mensaje-error" 
+          (querySelectorAll devuelve una lista de todos los elementos en la página que tienen la clase "mensaje-error" 
+          Si hay más de un elemento con esta clase, entonces mensajesError contendrá más de un elemento, 
+          y se puede acceder a cada elemento usando un índice (por ejemplo, mensajesError[0]), el array nos sirve para el siguiente paso
+      -si el mensaje que se encuentra en el array es mayor que 0, se elimina el mensaje. 
+          Esto con el fin de que no se acumulen los mensajes en la pagina y se muestre solo un mensaje
+      -se selecciona el form con querySelector y con .appendChild() agrega el elemento <p> debajo del form
+      -el elemento <p> ya creado en el html se le agrega la clase "mensaje-error" para que sea leida por el querySelectorAll y funcione bien el if del array
+      - */
   if (mensajeError) {
     const errorMensaje = document.createElement("p");
     errorMensaje.textContent = mensajeError;
